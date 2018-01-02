3d697ace52f9161b154d99c4397d940e03a4d573
derui <derutakayu@gmail.com>
2018-01-02 23:19:22 +0000

    Add infix operator for join class names

--- src/ocaml/renderer/sxfiler_classnames.ml
+++ src/ocaml/renderer/sxfiler_classnames.ml
@@ -3,6 +3,10 @@ let make list = Js.Optdef.return @@ Js.string @@ String.concat " " list
 
 let join (cls, condition) list = if condition then cls :: list else list
 
+module Infix = struct
+  let (<|>) = join
+end
+
 module Style = struct
   let grid_container = join ("grid", true)
   let grid_item = join ("grid__item", true)
