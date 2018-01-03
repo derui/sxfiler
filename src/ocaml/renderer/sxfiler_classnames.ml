8e05305cd39e728accbe4ece302d1a0822542576
derui <derutakayu@gmail.com>
2018-01-03 00:42:56 +0000

    Add more general styles

--- src/ocaml/renderer/sxfiler_classnames.ml
+++ src/ocaml/renderer/sxfiler_classnames.ml
@@ -4,10 +4,16 @@ let make list = Js.Optdef.return @@ Js.string @@ String.concat " " list
 let join (cls, condition) list = if condition then cls :: list else list
 
 module Infix = struct
-  let (<|>) = join
+  let (<|>) cls list = join list cls
 end
 
 module Style = struct
-  let grid_container = join ("grid", true)
-  let grid_item = join ("grid__item", true)
+  module Grid = struct
+    let container = ("grid", true)
+    let item = ("grid__item", true)
+    let item_row row = (Printf.sprintf "grid__item--row-%d" row, true)
+    let item_col col = (Printf.sprintf "grid__item--col-%d" col, true)
+    let item_span_row row span = (Printf.sprintf "grid__item--row-%d-span-%d" row span, true)
+    let item_span_col col span = (Printf.sprintf "grid__item--col-%d-span-%d" col span, true)
+  end
 end
