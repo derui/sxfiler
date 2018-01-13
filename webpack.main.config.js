20229226f2da28afc0011ffe934aa74d7a2fb464
derui <derutakayu@gmail.com>
2018-01-13 14:34:21 +0900

    Fix bundling renderer

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 module.exports = {
+  target: 'web',
   entry: './_build/default/src/ocaml/renderer/sxfiler_renderer.bc.js',
   output: {
     path: path.resolve(__dirname, 'dist', 'web'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: "commonjs2",
   },
-  node: {
-    fs: "empty",
-    path: "empty",
-  },
+  externals: [
+    "electron",
+    'fs',
+  ]
 };
