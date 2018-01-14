eae698d571f9361d111e1bc7585028e387260f3f
derui <derutakayu@gmail.com>
2018-01-14 14:04:30 +0900

    Add stats converter to json

--- src/ocaml/common/sxfiler_ffi.ml
+++ src/ocaml/common/sxfiler_ffi.ml
@@ -10,9 +10,64 @@ end
 (* FFI for fs and original-fs module *)
 module Fs = struct
   class type stat = object
-    method mode: Js.number Js.t Js.readonly_prop
-    method mtimeMs: float Js.readonly_prop
+    method dev: number t readonly_prop
+    method ino: number t readonly_prop
+    method mode: number t readonly_prop
+    method unlink: number t readonly_prop
+    method uid: number t readonly_prop
+    method gid: number t readonly_prop
+    method rdev: number t readonly_prop
+    method size: number t readonly_prop
+    method blksize: number t readonly_prop
+    method blocks: number t readonly_prop
+    method atime: date t readonly_prop
+    method mtime: date t readonly_prop
+    method ctime: date t readonly_prop
+    method birthtime: date t readonly_prop
     method isDirectory: unit -> bool t meth
+    method isFile: unit -> bool t meth
+    method isBlockDevice: unit -> bool t meth
+  end
+
+  class type stat_obj = object
+    method dev: number t readonly_prop
+    method ino: number t readonly_prop
+    method mode: number t readonly_prop
+    method unlink: number t readonly_prop
+    method uid: number t readonly_prop
+    method gid: number t readonly_prop
+    method rdev: number t readonly_prop
+    method size: number t readonly_prop
+    method blksize: number t readonly_prop
+    method blocks: number t readonly_prop
+    method atime: float readonly_prop
+    method mtime: float readonly_prop
+    method ctime: float readonly_prop
+    method birthtime: float readonly_prop
+    method isDirectory: bool t readonly_prop
+    method isFile: bool t readonly_prop
+    method isBlockDevice: bool t readonly_prop
+  end
+
+  (* Convert Stats class to pure json *)
+  let stat_to_obj t = object%js
+    val dev = t##.dev
+    val ino = t##.ino
+    val mode = t##.mode
+    val unlink = t##.unlink
+    val uid = t##.uid
+    val gid = t##.gid
+    val rdev = t##.rdev
+    val size = t##.size
+    val blksize = t##.blksize
+    val blocks = t##.blocks
+    val atime = t##.atime##getTime
+    val mtime = t##.mtime##getTime
+    val ctime = t##.ctime##getTime
+    val birthtime = t##.birthtime##getTime
+    val isDirectory = t##isDirectory ()
+    val isFile = t##isFile ()
+    val isBlockDevice = t##isBlockDevice ()
   end
 
   class type t = object
