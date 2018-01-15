88e8e735225eddbf2c93cfe5e783503153d81adb
derui <derutakayu@gmail.com>
2018-01-15 23:30:04 +0900

    Add new command to run app

---
+++ scripts/run-app.js
@@ -0,0 +1,12 @@
+require('./build-css.js');
+require('./build-ocaml.js');
+require('./bundle-main.js');
+require('./pre-package.js');
+
+const { execFileSync } = require('child_process');
+
+execFileSync('electron', ["./dist"], {stdio: 'inherit'}, (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+});
