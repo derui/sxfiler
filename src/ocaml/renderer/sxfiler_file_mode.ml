40d5b60ae51e5d1806154245b2da7280edd758ae
derui <derutakayu@gmail.com>
2018-01-17 16:32:46 +0900

    Show file permission like ls

--- src/ocaml/renderer/sxfiler_file_mode.ml
+++ src/ocaml/renderer/sxfiler_file_mode.ml
@@ -8,6 +8,60 @@ module Component = R.Component.Make_stateless (struct
     type t = _t Js.t
   end)
 
+module Mode_converter = struct
+  type capability = {
+    readable: bool;
+    writeable: bool;
+    executable: bool;
+  }
+  type t = {
+    file: bool;
+    directory: bool;
+    owner: capability;
+    group: capability;
+    other: capability
+  }
+
+  let bits_to_capability octal =
+    {
+      readable = octal land 4 = 4;
+      writeable = octal land 2 = 2;
+      executable = octal land 1 = 1;
+    }
+
+  let of_mode_bits mode_bits =
+    let bits_per_access = 3 in
+    let owner_bits = (mode_bits land 0o700) lsr (bits_per_access * 2)
+    and group_bits = (mode_bits land 0o70) lsr bits_per_access
+    and other_bits = mode_bits land 0o7
+    and file= mode_bits land 0o100000 = 0o100000
+    and directory= mode_bits land 0o040000 = 0o040000 in
+    {
+      file;
+      directory;
+      owner = bits_to_capability owner_bits;
+      group = bits_to_capability group_bits;
+      other = bits_to_capability other_bits;
+    }
+
+  let capability_to_string {readable;writeable;executable} =
+    let readable = if readable then "r" else "-"
+    and writeable = if writeable then "w" else "-"
+    and executable = if executable then "x" else "-" in
+    readable ^ writeable ^ executable
+
+  let to_string t =
+    let file_state = match (t.file, t.directory) with
+      | (_, true) -> "d"
+      | _ -> "-"
+    in
+    let owner = capability_to_string t.owner
+    and group = capability_to_string t.group
+    and other = capability_to_string t.other in
+    file_state ^ owner ^ group ^ other
+
+end
+
 let component = Component.make (fun props ->
     let mode = Js.float_of_number props##.mode in
     let mode = int_of_float mode in
@@ -17,6 +71,6 @@ let component = Component.make (fun props ->
         val className = Sxfiler_classnames.make ["fp-FileList_FileMode"]
       end)
       ~children:[|
-        R.text @@ string_of_int mode
+        R.text @@ Mode_converter.(of_mode_bits mode |> to_string)
       |]
   )
