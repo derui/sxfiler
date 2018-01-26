50e991e522b9cfeca5ee53203e6034072895c62e
derui <derutakayu@gmail.com>
2018-01-26 17:46:52 +0900

    Add utility for Option Monad

--- src/ocaml/common/sxfiler_common.ml
+++ src/ocaml/common/sxfiler_common.ml
@@ -3,3 +3,4 @@ module Ffi = Sxfiler_ffi
 module Event = Sxfiler_event
 module Message = Sxfiler_message
 module State = Sxfiler_state
+module Util = Sxfiler_util
--- src/ocaml/common/sxfiler_util.ml
+++ src/ocaml/common/sxfiler_util.ml
@@ -1,2 +1,18 @@
 (* Utilities for binding js_of_ocaml *)
 
+module Option = struct
+  let map ~f v = match v with
+    | None -> None
+    | Some v -> Some (f v)
+
+  let bind v f = match v with
+    | None -> None
+    | Some v -> f v
+
+  let return v = Some v
+
+  module Infix = struct
+    let (>>=) = bind
+    let return = return
+  end
+end
