766326ad8745e4cf4f8fb16d2cf125c5e3f7d312
derui <derutakayu@gmail.com>
2018-01-28 13:08:14 +0900

    fix: add reaction module

---
+++ src/ocaml/main/sxfiler_message_reaction.ml
@@ -0,0 +1,48 @@
+
+module E = Sxfiler_common.Event
+module FFI = Sxfiler_common.Ffi
+module M = Sxfiler_common.Message
+
+exception Unhandled_promise
+
+module type Fs = sig
+  val resolve: unit -> FFI.Fs.t Js.t
+end
+
+module type S = sig
+  module Fs : Fs
+
+  val request_files_in_directory: string -> M.t Lwt.t
+end
+
+module Make(Fs:Fs) : S with module Fs = Fs = struct
+  module Fs = Fs
+
+  (** Handle request_files_in_directory message *)
+  let request_files_in_directory path =
+    let module File_list = Sxfiler_file_list in
+    let module M = Sxfiler_modules in
+    let fs = Fs.resolve () in
+    let path_ = M.path in
+    let path = Js.string path in
+    let absolute = path_##resolve Js.(array [|path|]) |> Js.to_string in
+
+    let open Lwt.Infix in
+    let lwt = File_list.get_file_stats ~fs absolute
+      >>= (fun files ->
+          let module M = Sxfiler_common.Message in
+          Lwt.return @@ M.finish_files_in_directory (None, absolute, files)
+        )
+    in
+
+    let lwt = Lwt.catch (fun () -> lwt) (fun err ->
+        Firebug.console##log err;
+        match err with
+        | File_list.Not_directory f ->
+          let module M = Sxfiler_common.Message in
+          Lwt.return @@ M.finish_files_in_directory (Some err, absolute, [||])
+        | _ -> raise Unhandled_promise
+      )
+    in
+    lwt
+end
