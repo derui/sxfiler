91fe7aa3de159cf8312db7b66c1fa8147b86deb8
derui <derutakayu@gmail.com>
2018-02-06 18:18:46 +0900

    feat: add new type to store information of pane

--- src/ocaml/common/sxfiler_types.ml
+++ src/ocaml/common/sxfiler_types.ml
@@ -43,3 +43,45 @@ module File_stat = struct
     link_path = Js.Optdef.map (js##.link_path) Js.to_string |> Js.Optdef.to_option;
   }
 end
+
+module Pane_position = struct
+  type t = Left | Right
+end
+
+module Pane = struct
+  type t = {
+    position: Pane_position.t;
+    directory: string;
+    file_list: File_stat.t list;
+    cursor_pos: current_cursor;
+  }
+
+  class type js = object
+    method position: Pane_position.t Js.readonly_prop
+    method directory: Js.js_string Js.t Js.readonly_prop
+    method fileList: File_stat.js Js.t Js.js_array Js.t Js.readonly_prop
+    method cursorPos : Js.number Js.t Js.readonly_prop
+  end
+
+  let make ~position ~file_list ~cursor_pos ~directory =
+    {
+      position;
+      directory;
+      file_list;
+      cursor_pos;
+    }
+
+  let to_js : t -> js Js.t = fun t -> object%js
+    val position = t.position
+    val fileList = List.map File_stat.to_js t.file_list |> Array.of_list |> Js.array
+    val directory = Js.string t.directory
+    val cursorPos = Js.number_of_float @@ float_of_int t.cursor_pos
+  end
+
+  let of_js : js Js.t -> t = fun js -> {
+    position = js##.position;
+    directory = Js.to_string js##.directory;
+    file_list = Js.to_array js##.fileList |> Array.map File_stat.of_js |> Array.to_list;
+    cursor_pos = int_of_float @@ Js.float_of_number js##.cursorPos;
+  }
+end
