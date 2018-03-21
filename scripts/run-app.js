72e715e06177536d719c8781d26f977346bfd769
derui <derutakayu@gmail.com>
2018-03-21 12:09:07 +0900

    chore: bundle dll in run app

--- scripts/run-app.js
+++ scripts/run-app.js
@@ -2,6 +2,7 @@ require('./pre-package.js');
 
 const { buildCssWithWatch, buildCss } = require('./build-css.js');
 const { buildOCamlWithWatch, buildOCaml } = require('./build-ocaml.js');
+const { bundleDll } = require('./build-dll.js');
 
 const { spawn } = require('child_process');
 
@@ -22,6 +23,7 @@ module.exports = runApp;
 
 if (require.main === module) {
   (function() {
+    bundleDll();
     buildCss();
     buildOCaml();
 
