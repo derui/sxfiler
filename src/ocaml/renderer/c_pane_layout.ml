d6761558468d7f1ce163f407d66a797a4cc4a2ad
derui <derutakayu@gmail.com>
2018-04-02 22:24:03 +0900

    feat: refine file list styles

--- src/ocaml/renderer/c_pane_layout.ml
+++ src/ocaml/renderer/c_pane_layout.ml
@@ -9,6 +9,10 @@ module Component = R.Component.Make_stateless (struct
     end
   end)
 
+let spacer =
+  let class_name = "fp-PaneLayout_Spacer" in
+  R.Dom.of_tag `div ~key:"spacer" ~props:(R.element_spec ~class_name ())
+
 let component = Component.make (fun props ->
     let state = props##.state in
     let module P = T.Pane in
@@ -24,6 +28,7 @@ let component = Component.make (fun props ->
     and right_pane = create_pane_element "right" right_pane @@ C.State.is_right_active state in
     let children = [|
       left_pane;
+      spacer;
       right_pane;
       R.create_element ~key:"operations" ~props:(object%js
         val operationLog = state.C.State.operation_log
