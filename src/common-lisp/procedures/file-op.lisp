c749fc2b5d25a9cf09004eb8fa0852ea6bfc26b1
derui <derutakayu@gmail.com>
2018-05-02 11:34:11 +0900

    feat: add function to delete item that apply file and directory

--- src/common-lisp/procedures/file-op.lisp
+++ src/common-lisp/procedures/file-op.lisp
@@ -24,16 +24,13 @@
   "Wrap operations to use focused item in src-pane."
   (if (= 0 (length body))
       '()
-      (alexandria:with-gensyms (file-list focused-item find-file)
+      (alexandria:with-gensyms (file-list focused-item)
         `(let ((,file-list (sxfiler/types/pane:pane-file-list ,src-pane))
                (,focused-item (sxfiler/types/pane:pane-focused-item ,src-pane)))
-           (flet ((,find-file (item)
-                    (when item
-                      (find-if (lambda (v) (file-stat-equal item v)) ,file-list))))
-
-             (if (funcall #',find-file ,focused-item)
-                 ,@body
-                 (error (make-condition 'not-found-file-id-error :id (file-stat-id ,focused-item)))))))))
+           (if (and ,focused-item
+                    (find-if (lambda (v) (file-stat-equal ,focused-item v)) ,file-list))
+               ,@body
+               (error (make-condition 'not-found-file-id-error :id (file-stat-id ,focused-item))))))))
 
 ;; Conditions
 (define-condition not-found-file-id-error (error)
@@ -45,7 +42,7 @@
 
 ;; Primitive functions are used with pathname.
 (defun copy-file (src dest)
-  "simply coping file from SRC to DIST.
+  "simply coping a file from SRC to DIST.
 This function has side effect."
   (check-type src pathname)
   (check-type dest pathname)
@@ -54,11 +51,23 @@ This function has side effect."
                   (namestring dest)))
 
 (defun move-file (src dest &key (overwrite t))
-  "simply move file from SRC to DIST. This function has side effect."
+  "simply move a file from SRC to DIST. This function has side effect."
   (check-type src pathname)
   (check-type dest pathname)
 
   (uiop:rename-file-overwriting-target src dest))
+
+(defun delete-file (file)
+  "simple delete a FILE. this function has side effect"
+  (check-type file pathname)
+
+  (uiop:delete-file-if-exists file))
+
+(defun delete-directory-tree (directory &key validate-to-delete)
+  "simple delete a tree of directory. This function has side effect"
+  (check-type directory pathname)
+
+  (uiop:delete-directory-tree directory :validate validate-to-delete))
 
 ;; functions are related current focusing item.
 (defun copy-file-to-another-pane (src-pane dest-pane)
@@ -87,6 +96,19 @@ This function has side effect."
         (move-file (uiop:merge-pathnames* filename src-dir)
                    (uiop:merge-pathnames* filename dest-dir))))))
 
+(defun delete-focused-item (pane)
+  "Delete a file current focused file in PANE."
+  (check-type pane sxfiler/types/pane:pane)
+
+  (with-focused-item (pane)
+    (let ((dir (pane-directory pane))
+          (focused-item (sxfiler/types/pane:pane-focused-item pane)))
+      (let ((filename (sxfiler/types/file-stat:file-stat-filename focused-item))
+            (dirname (sxfiler/types/file-stat:file-stat-directory focused-item)))
+        (if (sxfiler/types/file-stat:file-stat-directory-p focused-item)
+            (delete-directory-tree (uiop:merge-pathnames* "" dirname))
+            (delete-file (uiop:merge-pathnames* filename dir)))))))
+
 ;; functions are related state. These functions should use from API
 (defun copy-file-from-active-to-inactive (state)
   "copy file focusing on active pane of state to inactive pane of state."
@@ -96,8 +118,6 @@ This function has side effect."
     (copy-file-to-another-pane active-pane inactive-pane)
     (sxfiler/state:set-inactive-pane state (renew-file-list inactive-pane))))
 
-
-
 (defun move-file-from-active-to-inactive (state)
   "Move file focising on active pane in state to inactive pane in state."
   (check-type state sxfiler/state:state)
@@ -105,6 +125,15 @@ This function has side effect."
   (multiple-value-bind (active-pane inactive-pane) (panes-of-state state)
     (move-file-to-another-pane active-pane inactive-pane)
     (sxfiler/state:set-inactive-pane state (renew-file-list inactive-pane))))
+
+(defun delete-focused-item-from-active (state)
+  "Delete focusing item on active pane in state."
+  (check-type state sxfiler/state:state)
+
+  (multiple-value-bind (active-pane inactive-pane) (panes-of-state state)
+    (declare (ignorable inactive-pane))
+    (delete-focused-item active-pane)
+    (sxfiler/state:set-active-pane state (renew-file-list active-pane))))
 
 ;; utility to open some dialog.
 (defun open-confirm-dialog (state handle)
@@ -120,7 +149,17 @@ User must call '/dialog/confirmation' method after this.
 
 (defun expose (server)
   "expose functions for file-related operations to JSON-RPC"
-  (jsonrpc:expose server "/file/movefile"
+  (jsonrpc:expose server "/file/delete"
+                  (lambda (args)
+                    (check-type args hash-table)
+                    (let ((force-delete (gethash "force" args)))
+                      (with-root-state (state)
+                        (if force-delete
+                            (delete-focused-item-from-active state)
+                            (open-confirm-dialog state (lambda ()
+                                                         (with-root-state (state)
+                                                           (delete-focused-item-from-active state)))))))))
+  (jsonrpc:expose server "/file/move"
                   (lambda (args)
                     (check-type args hash-table)
                     (let ((force-move (gethash "force" args)))
@@ -130,7 +169,7 @@ User must call '/dialog/confirmation' method after this.
                             (open-confirm-dialog state (lambda ()
                                                          (with-root-state (state)
                                                            (move-file-from-active-to-inactive state)))))))))
-  (jsonrpc:expose server "/file/copyFile"
+  (jsonrpc:expose server "/file/copy"
                   (lambda (args)
                     (check-type args hash-table)
                     (let ((force-copy (gethash "force" args)))
