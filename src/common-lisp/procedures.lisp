c0bc365e3afce0cc6b6f668970e2ab068f69df6e
derui <derutakayu@gmail.com>
2018-05-10 14:37:53 +0900

    feat: add functions to server for config

--- src/common-lisp/procedures.lisp
+++ src/common-lisp/procedures.lisp
@@ -7,6 +7,7 @@
   (:import-from #:sxfiler/procedures/file-op)
   (:import-from #:sxfiler/procedures/pane-op)
   (:import-from #:sxfiler/procedures/completer-op)
+  (:import-from #:sxfiler/procedures/config-op)
   (:export #:expose-procedures))
 
 (in-package #:sxfiler/procedures)
@@ -15,6 +16,7 @@
   (sxfiler/procedures/file-op:expose server)
   (sxfiler/procedures/pane-op:expose server)
   (sxfiler/procedures/completer-op:expose server)
+  (sxfiler/procedures/config-op:expose server)
   (jsonrpc:expose server "getCurrentState" (lambda (args)
                                              (declare (ignorable args))
                                              (with-root-state (state)
---
+++ src/common-lisp/procedures/config-op.lisp
@@ -0,0 +1,37 @@
+;;; exposable functions for operations of pane.
+(in-package :cl-user)
+(defpackage #:sxfiler/procedures/config-op
+  (:use #:cl)
+  (:import-from #:sxfiler/config)
+  (:export #:expose))
+(in-package #:sxfiler/procedures/config-op)
+
+(defparameter *config* (sxfiler/config:make-config)
+  "Global configuration.")
+
+(defparameter *config-lock* (sb-thread:make-mutex)
+  "Mutex for lock configuration stored at *config*.")
+
+(defmacro with-config ((var) &body body)
+  `(if (and (sb-thread:holding-mutex-p *config-lock*)
+            (eql sb-thread:*current-thread* (sb-thread:mutex-owner *config-lock*)))
+       (let ((,var *config*))
+         (progn
+           ,@body))
+       (sb-thread:with-mutex (*config-lock*)
+         (let ((,var *config*))
+           (progn
+             ,@body)))))
+
+(defun expose (server)
+  (check-type server jsonrpc:server)
+  (jsonrpc:expose server "config/current" #'(lambda (args)
+                                              (declare (ignorable args))
+                                              (with-config (config)
+                                                config)))
+  (jsonrpc:expose server "config/load" #'(lambda (args)
+                                           (declare (ignorable args))
+                                           (with-config (config)
+                                             (with-open-file (stream "default.json")
+                                               (setf *config* (sxfiler/config:load-from-stream stream))
+                                               *config*)))))
