dbfa19e171af1e18d945325fea740e202eea4345
derui <derutakayu@gmail.com>
2018-06-27 23:40:04 +0900

    refactor: Use ppx_deriving_yojson instead of hand-writing

--- src/ocaml/types/yojson/configuration.ml
+++ src/ocaml/types/yojson/configuration.ml
@@ -4,8 +4,21 @@ open Sxfiler_types.Configuration
 module Key_maps = struct
   include Key_maps
 
-  let to_js : t -> Yojson.json = fun t ->
-    `Assoc [("fileList", Key_map.to_js t.file_list)]
+  module Js = struct
+    type t = {
+      file_list: Yojson.Safe.json;
+    } [@@deriving yojson]
+  end
+
+  let to_js : t -> Yojson.Safe.json = fun t ->
+    Js.to_yojson {Js.file_list = Key_map.to_js t.file_list}
+
+  let of_js : Yojson.Safe.json -> t = fun json ->
+    match Js.of_yojson json with
+    | Error _ -> Printf.eprintf "TODO parse error"; empty
+    | Ok js -> {
+        file_list = Key_map.of_js js.file_list;
+      }
 end
 
 (** [Viewer] provides configuration for viewer. This configuration will not manage
@@ -14,13 +27,33 @@ end
 module Viewer = struct
   include Viewer
 
-  let to_js : t -> Yojson.json = fun t ->
+  module Js = struct
+    type t = {
+      current_stack_name:string;
+      stack_layout:string;
+      key_maps:Yojson.Safe.json;
+    } [@@deriving yojson]
+  end
+
+  let to_js : t -> Yojson.Basic.json = fun t ->
     let module T = Sxfiler_types.Types in
-    `Assoc [
-      ("currentStackName", `String t.current_stack_name);
-      ("stackLayout", `String (T.Layout.to_string t.stack_layout));
-      ("keyMaps", Key_maps.to_js t.key_maps);
-    ]
+    let js = Js.{
+        current_stack_name = t.current_stack_name;
+        stack_layout = T.Layout.to_string t.stack_layout;
+        key_maps = Key_maps.to_js t.key_maps;
+      }
+    in Js.to_yojson js |> Yojson.Safe.to_basic
+
+  let of_js : Yojson.Safe.json -> t = fun json ->
+    match Js.of_yojson json with
+    | Error _ -> failwith "TODO parse error"
+    | Ok js ->
+      let module T = Sxfiler_types.Types in
+      {
+        current_stack_name = js.Js.current_stack_name;
+        stack_layout = T.Layout.of_string js.Js.stack_layout;
+        key_maps = Key_maps.of_js js.key_maps;
+      }
 end
 
 (** [Server] provides configuration for server. This configuration will not manage
@@ -29,9 +62,24 @@ end
 module Server = struct
   include Server
 
-  let to_js : t -> Yojson.json = fun t ->
+  module Js = struct
+    type t = {
+      sort_order:string;
+    } [@@deriving yojson]
+  end
+
+  let to_js : t -> Yojson.Safe.json = fun t ->
     let module T = Sxfiler_types.Types in
     `Assoc [
       ("sortOrder", `String (T.Sort_type.to_string t.sort_order))
     ]
+
+  let of_js : Yojson.Safe.json -> t = fun json ->
+    match Js.of_yojson json with
+    | Error _ -> failwith "TODO: handle parse error"
+    | Ok js ->
+      let module T = Sxfiler_types.Types in
+      {
+        sort_order = T.Sort_type.of_string js.Js.sort_order;
+      }
 end
--- src/ocaml/types/yojson/key_map.ml
+++ src/ocaml/types/yojson/key_map.ml
@@ -1,6 +1,15 @@
 include Sxfiler_types.Key_map
 
-let to_js : t -> Yojson.json = fun t ->
+let to_js : t -> Yojson.Safe.json = fun t ->
   let module CA = Sxfiler_types.Callable_action in
   let key_value = dump t |> List.map (fun (k, v) -> (k, CA.to_string v)) in
   `Assoc (List.map (fun (k, v) -> (k, `String v)) key_value)
+
+let of_js : Yojson.Safe.json -> t = fun json ->
+  let module CA = Sxfiler_types.Callable_action in
+  match json with
+  | `Assoc values -> List.fold_left (fun keymap (key, value) ->
+      let value = Yojson.Safe.Util.to_string value in
+      add keymap ~key ~action:(CA.of_string value)
+    ) empty values
+  | _ -> Printf.printf "Invalid keymap"; empty
