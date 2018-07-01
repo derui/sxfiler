145c5ae5461c21a689dc64c2c363077baaf89ffe
derui <derutakayu@gmail.com>
2018-07-01 14:30:32 +0900

    feat: add Core library for Sxfiler

---
+++ src/ocaml/core/option.ml
@@ -0,0 +1,20 @@
+
+(** [is_none v] return [v] is {!None} or not *)
+let is_none = function
+  | None -> true
+  | Some _ -> false
+
+(** [is_some v] return [v] is {!Some} or not *)
+let is_some v = not @@ is_none v
+
+exception Not_some
+
+(** [get_exn v] return wrapped value of [v]. Raise {!Not_some} if [v] is None.  *)
+let get_exn = function
+  | None -> raise Not_some
+  | Some v -> v
+
+(** [get ~default v] return wrapped value of [v]. If [v] is None, this returns [default]. *)
+let get ~default v = function
+  | None -> default
+  | Some v -> v
