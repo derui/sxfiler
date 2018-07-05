3cc5fa5b02373b7c2b592c6662e5026256c54b20
derui <derutakayu@gmail.com>
2018-07-05 10:37:56 +0900

    feat: implement completion for file nodes

--- src/ocaml/server/completion_op.ml
+++ src/ocaml/server/completion_op.ml
@@ -1,10 +1,17 @@
 (** Completion module defines functions for RPC of completion. *)
+module SC = Sxfiler_server_core
 module Rpc = Jsonrpc_ocaml_yojson
 module T = Sxfiler_types
 module Ty = Sxfiler_types_yojson
 
 (** prefixes of completion module. *)
 let module_prefixes = ["completion"]
+
+module File_source = SC.Statable.Make(struct
+    type t = T.Node.t list
+
+    let empty () = []
+  end)
 
 module Setup_file_sync = Procedure_intf.Make(struct
   include T.Rpc.Completion.Setup_file_sync
@@ -13,7 +20,17 @@ module Setup_file_sync = Procedure_intf.Make(struct
   let param_of_json = `Required param_of_yojson
   let result_to_json = `Void
 
-  let handle param = failwith "not implemented yet"
+  let handle param =
+    let module R = SC.Root_state in
+    File_source.with_lock (fun source ->
+        let%lwt state = Global.Root.get () in
+        let ws = match R.find_workspace state ~name:param.workspace_name with
+          | None -> raise Rpc.Types.(Jsonrpc_error Error_code.Invalid_request)
+          | Some ws -> ws
+        in
+        let nodes = ws.T.Workspace.current.T.Tree_snapshot.nodes in
+        File_source.update nodes
+      )
 end)
 
 (** [Read_file] module provides to complete current sources with inputted string from parameter, then
@@ -26,28 +43,48 @@ module Read_file_sync = Procedure_intf.Make(struct
   let param_of_json = `Required param_of_yojson
   let result_to_json = `Result result_to_yojson
 
-  let handle param = failwith "not implemented yet"
+  let handle param =
+    let module Comp =  Sxfiler_server_completion.Completer in
+    let%lwt completer = Global.Completion.get () in
+    match completer with
+    | None -> raise Rpc.Types.(Jsonrpc_error Error_code.Internal_error)
+    | Some completer ->
+      File_source.with_lock (fun collection ->
+          let module S = (struct
+            type t = T.Node.t
+            let to_string n = n.T.Node.full_path
+          end) in
+          let candidates = Comp.read completer ~input:param.input ~collection ~stringify:(module S) in
+
+          List.map (fun c -> T.Types.Candidate.{
+              start = c.Comp.start;
+              length = c.Comp.length;
+              value = c.Comp.value;
+            }) candidates
+          |> Array.of_list
+          |> Lwt.return
+        )
 end)
 
 module Read_directory_sync = Procedure_intf.Make(struct
-  include T.Rpc.Completion.Read_directory_sync
-  open Ty.Rpc.Completion.Read_directory_sync
+    include T.Rpc.Completion.Read_directory_sync
+    open Ty.Rpc.Completion.Read_directory_sync
 
-  let param_of_json = `Required param_of_yojson
-  let result_to_json = `Result result_to_yojson
+    let param_of_json = `Required param_of_yojson
+    let result_to_json = `Result result_to_yojson
 
-  let handle param = failwith "not implemented yet"
-end)
+    let handle param = failwith "not implemented yet"
+  end)
 
 module Read_history_sync = Procedure_intf.Make(struct
-  include T.Rpc.Completion.Read_history_sync
-  open Ty.Rpc.Completion.Read_history_sync
+    include T.Rpc.Completion.Read_history_sync
+    open Ty.Rpc.Completion.Read_history_sync
 
-  let param_of_json = `Required param_of_yojson
-  let result_to_json = `Result result_to_yojson
+    let param_of_json = `Required param_of_yojson
+    let result_to_json = `Result result_to_yojson
 
-  let handle param = failwith "not implemented yet"
-end)
+    let handle param = failwith "not implemented yet"
+  end)
 
 let read param = failwith "not implemented yet"
 
