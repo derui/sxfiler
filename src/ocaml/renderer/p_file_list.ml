6e58f67e558a2f360cb4fc4f00b07784ecfd20a0
derui <derutakayu@gmail.com>
2018-08-28 16:38:10 +0900

    feat: allow to move filer to parent directory

--- src/ocaml/renderer/p_file_list.ml
+++ src/ocaml/renderer/p_file_list.ml
@@ -38,84 +38,85 @@ let content =
              end
            end)
 
-    ~spec:(R.component_spec
-             ~constructor:(fun this _ -> this##.nodes := Jstable.create ())
-             ~initial_custom:(fun _ _ -> object%js end)
-             ~initial_state:(fun _ _ ->
-                 object%js
-                   val virtualizedList = VL.make ~item_height ()
-                 end
-               )
-             ~component_did_mount:(fun this ->
-                 let vt = this##.props##.filer in
-                 let vl = this##.state##.virtualizedList in
-                 let open Option.Infix in
-                 ignore (
-                   R.Ref_table.find ~key:key_of_filelist this##.nodes >|= fun e ->
-                   let pos = this##.props##.selectedItemIndex in
-                   this##setState (object%js
-                     val virtualizedList = VL.update_list_height e##.clientHeight vl
-                                           |> VL.update_all_items @@ Array.of_list vt.Vt.nodes
-                                           |> VL.recalculate_visible_window pos
-                   end))
-               )
-             ~component_will_receive_props:(fun this _ ->
-                 let vt = this##.props##.filer in
-                 ignore (
-                   let items = Array.of_list vt.Vt.nodes in
-                   let vl = VL.update_all_items items this##.state##.virtualizedList in
-                   let vl = VL.recalculate_visible_window this##.props##.selectedItemIndex vl
-                   in
-                   this##setState (object%js
-                     val virtualizedList = vl
-                   end)
-                 )
-               )
-             (fun this ->
-                let vl = this##.state##.virtualizedList in
-
-                let items = VL.get_items_in_window vl in
-
-                let children = Array.mapi (fun index item ->
-                    let module N = T.Node in
-                    [%c P_file_item.t ~key:(item.N.name)
-                        ~props:(object%js
-                          val item = item
-                          val selected = (index = this##.props##.selectedItemIndex)
-                          val focused = this##.props##.focused
-                        end)
-                    ]
-                  ) items |> Array.to_list
-                in
-                let scroll_bar =
-                  let start, size = VL.percentage_by_visible this##.state##.virtualizedList in
-                  [%c C_scroll_bar.t ~key:"scroll-bar" ~props:(object%js
-                      val start = start
-                      val windowSize = size
-                    end)]
-                in
-                let resize_sensor = R.create_element ~key:"resize-sensor"
-                    ~props:(object%js
-                      val getParentSize = (fun () ->
-                          let open Option.Infix in
-                          let size = R.Ref_table.find ~key:key_of_filelist this##.nodes >|= (fun e ->
-                              let rect = e##getBoundingClientRect in
-                              let height = int_of_float @@ rect##.bottom -. rect##.top
-                              and width = int_of_float @@ rect##.right -. rect##.left in
-                              {C_resize_sensor.height;width})
-                          in
-                          Option.get ~default:(fun () -> {C_resize_sensor.height = 0;width = 0}) size)
-                      val onResized = (fun size ->
-                          let height = size.C_resize_sensor.height in
-                          this##setState (object%js
-                            val virtualizedList = VL.update_list_height height vl
-                          end))
-                    end) C_resize_sensor.t
-                in
-                let children = List.concat [children;[resize_sensor;scroll_bar]] in
-                let _ref e = R.Ref_table.add ~key:key_of_filelist ~value:e this##.nodes in
-                [%e ul ~_ref ~class_name:"fp-FileList_Content"  children])
+    ~spec:(
+      R.component_spec
+        ~constructor:(fun this _ -> this##.nodes := Jstable.create ())
+        ~initial_custom:(fun _ _ -> object%js end)
+        ~initial_state:(fun _ _ ->
+            object%js
+              val virtualizedList = VL.make ~item_height ()
+            end
           )
+        ~component_did_mount:(fun this ->
+            let vt = this##.props##.filer in
+            let vl = this##.state##.virtualizedList in
+            let open Option.Infix in
+            ignore (
+              R.Ref_table.find ~key:key_of_filelist this##.nodes >|= fun e ->
+              let pos = this##.props##.selectedItemIndex in
+              this##setState (object%js
+                val virtualizedList = VL.update_list_height e##.clientHeight vl
+                                      |> VL.update_all_items @@ Array.of_list vt.Vt.nodes
+                                      |> VL.recalculate_visible_window pos
+              end))
+          )
+        ~component_will_receive_props:(fun this new_props ->
+            let vt = new_props##.filer in
+            ignore (
+              let items = Array.of_list vt.Vt.nodes in
+              let vl = VL.update_all_items items this##.state##.virtualizedList in
+              let vl = VL.recalculate_visible_window new_props##.selectedItemIndex vl
+              in
+              this##setState (object%js
+                val virtualizedList = vl
+              end)
+            )
+          )
+        (fun this ->
+           let vl = this##.state##.virtualizedList in
+
+           let items = VL.get_items_in_window vl in
+
+           let children = Array.mapi (fun index item ->
+               let module N = T.Node in
+               [%c P_file_item.t ~key:(item.N.name)
+                   ~props:(object%js
+                     val item = item
+                     val selected = (index = this##.props##.selectedItemIndex)
+                     val focused = this##.props##.focused
+                   end)
+               ]
+             ) items |> Array.to_list
+           in
+           let scroll_bar =
+             let start, size = VL.percentage_by_visible this##.state##.virtualizedList in
+             [%c C_scroll_bar.t ~key:"scroll-bar" ~props:(object%js
+                 val start = start
+                 val windowSize = size
+               end)]
+           in
+           let resize_sensor = R.create_element ~key:"resize-sensor"
+               ~props:(object%js
+                 val getParentSize = (fun () ->
+                     let open Option.Infix in
+                     let size = R.Ref_table.find ~key:key_of_filelist this##.nodes >|= (fun e ->
+                         let rect = e##getBoundingClientRect in
+                         let height = int_of_float @@ rect##.bottom -. rect##.top
+                         and width = int_of_float @@ rect##.right -. rect##.left in
+                         {C_resize_sensor.height;width})
+                     in
+                     Option.get ~default:(fun () -> {C_resize_sensor.height = 0;width = 0}) size)
+                 val onResized = (fun size ->
+                     let height = size.C_resize_sensor.height in
+                     this##setState (object%js
+                       val virtualizedList = VL.update_list_height height vl
+                     end))
+               end) C_resize_sensor.t
+           in
+           let children = List.concat [children;[resize_sensor;scroll_bar]] in
+           let _ref e = R.Ref_table.add ~key:key_of_filelist ~value:e this##.nodes in
+           [%e ul ~_ref ~class_name:"fp-FileList_Content"  children])
+    )
 
 (* wrapping component. *)
 let t = R.Component.make_stateless
