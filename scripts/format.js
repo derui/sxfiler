ade02b69050a8e521b4df155eb2f59c5514771df
derui <derutakayu@gmail.com>
2018-08-29 22:41:24 +0900

    chore: improve formatting time

--- scripts/format.js
+++ scripts/format.js
@@ -1,13 +1,24 @@
-const { execFileSync } = require('child_process');
+const util = require('util');
+
+let { execFileSync, execFile } = require('child_process');
+execFile = util.promisify(execFile);
 
 const glob = require('glob');
 
 glob('{src,test}/**/*.{ml,mli}', (er, files) => {
-  files.forEach(f => {
+  async function formatting(f) {
     console.log(`Formatting OCaml source: ${f}`);
-    execFileSync('ocamlformat', ['-i', f], { stdio: 'inherit' });
-    execFileSync('ocp-indent', ['-i', f], { stdio: 'inherit' });
-  });
+    const ocamlFormatResult = await execFile('ocamlformat', ['-i', f]);
+    const ocpIndentResult = await execFile('ocp-indent', ['-i', f]);
+    if (ocamlFormatResult.stdout || ocamlFormatResult.stderr) {
+      console.log(ocamlFormatResult.stdout, ocamlFormatResult.stderr);
+    }
+
+    if (ocpIndentResult.stdout || ocpIndentResult.stderr) {
+      console.log(ocpIndentResult.stdout, ocpIndentResult.stderr);
+    }
+  }
+  Promise.all(files.map(formatting));
 });
 
 execFileSync(
