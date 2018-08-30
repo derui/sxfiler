116fd25b7286689cec905de63f17f277ae4b10bf
derui <derutakayu@gmail.com>
2018-08-30 10:50:07 +0900

    chore: use 'diff' action to promote module

---
+++ src/ocaml/renderer/command/sxfiler_renderer_command.ml
@@ -0,0 +1,14 @@
+include Core
+module Completion = S_completion
+
+module File_list = S_file_list
+
+module Global = S_global
+
+let expose_static registry hub = 
+let registry = S_completion.expose registry hub in
+let registry = S_file_list.expose registry hub in
+S_global.expose registry hub
+let expose_dynamic registry hub = 
+registry
+[@@warning "-27"]
