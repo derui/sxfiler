feb13a9181b5a7e0b6e2ab14998046040785dc3e
derui <derutakayu@gmail.com>
2018-09-23 10:52:23 +0900

    feat: add endpoint to move nodes

--- src/ocaml/server_lib/proc_filer.ml
+++ src/ocaml/server_lib/proc_filer.ml
@@ -72,6 +72,22 @@ module Enter_directory (Gateway : G.Filer.Enter_directory.S) = struct
     | {filer = Some s; _} -> Lwt.return s
 end
 
+module Move_nodes (Gateway : G.Filer.Move_nodes.S) = struct
+  type params = Gateway.params
+  type result = unit
+
+  let params_of_json = `Required Gateway.params_of_yojson
+  let result_to_json _ = `Null
+
+  let handle params =
+    let%lwt result = Gateway.handle params in
+    let module J = Jsonrpc_ocaml_yojson in
+    let module EF = Sxfiler_rpc.Errors.Filer in
+    match result with
+    | {Gateway.not_found_workbench = true; _} -> J.(Exception.raise_error EF.not_found_workbench)
+    | _ -> Lwt.return_unit
+end
+
 let expose server =
   let module S = Jsonrpc_ocaml_yojson.Server in
   let module W = Sxfiler_usecase.Filer in
@@ -99,10 +115,16 @@ let expose server =
       (U.Filer.Enter_directory (Filer_repo) (I.Location_scanner_service) (Global.Clock)) in
   let module Enter_directory = Procedure_intf.Make (Enter_directory (Enter_directory_gateway)) in
   let module E = Sxfiler_rpc.Endpoints in
+  let module Move_nodes_gateway =
+    G.Filer.Move_nodes.Make
+      (U.Filer.Move_nodes.Make (Filer_repo) (Wb_repo) (I.Location_scanner_service)
+         (I.Node_transporter_service)) in
+  let module Move_nodes = Procedure_intf.Make (Move_nodes (Move_nodes_gateway)) in
   List.fold_left
     (fun server (name, handler) -> S.expose ~_method:name ~handler server)
     server
     [ (E.Filer.Make.endpoint, Make.handler)
     ; (E.Filer.Get.endpoint, Get.handler)
     ; (E.Filer.Move_parent.endpoint, Move_parent.handler)
-    ; (E.Filer.Enter_directory.endpoint, Enter_directory.handler) ]
+    ; (E.Filer.Enter_directory.endpoint, Enter_directory.handler)
+    ; (E.Filer.Move_nodes.endpoint, Move_nodes.handler) ]
