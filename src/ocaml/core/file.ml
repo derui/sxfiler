634af3f16a1d095d28e96a278067252fe02032e4
derui <derutakayu@gmail.com>
2018-09-23 10:25:21 +0900

    feat: add module to manage file

---
+++ src/ocaml/core/file.ml
@@ -0,0 +1,35 @@
+let mk_temp_dir ?mode ?dir pat =
+  let dir = Option.get ~default:Filename.get_temp_dir_name dir
+  and mode = Option.get ~default:Fun.(const 0o755) mode in
+  let make_pattern () =
+    let rand = Random.int 0x1000000 in
+    Printf.sprintf "%s%06x" pat rand
+  in
+  let rec loop count =
+    let pat = make_pattern () in
+    let temp_dir = Filename.concat dir pat in
+    match Unix.mkdir temp_dir mode with
+    | () -> temp_dir
+    | exception Unix.Unix_error (Unix.EEXIST, _, _) -> loop (pred count)
+    | exception Unix.Unix_error (Unix.EINTR, _, _) -> loop count
+    | exception Unix.Unix_error (e, _, _) ->
+      failwith (Printf.sprintf "mkd_temp_dir: %s" (Unix.error_message e))
+  in
+  loop 1000
+
+let remove ?(recursive = true) path =
+  let open Fun in
+  if not & Sys.file_exists path then ()
+  else if not recursive then Sys.remove path
+  else
+    let rec remove_all_structure paths =
+      match paths with
+      | [] -> ()
+      | path :: rest ->
+        if Sys.is_directory path then (
+          let entries = Sys.readdir path |> Array.map (Filename.concat path) |> Array.to_list in
+          remove_all_structure entries ; Unix.rmdir path )
+        else Sys.remove path ;
+        remove_all_structure rest
+    in
+    remove_all_structure [path]
---
+++ src/ocaml/core/file.mli
@@ -0,0 +1,13 @@
+(** File module provides utility functions for file management such as creating temporary directory. *)
+
+val mk_temp_dir : ?mode:int -> ?dir:string -> string -> string
+(** [with_temp_dir ?mode ?dir pat] returns name of temporary directory in [dir].
+    Using {!Filename.get_temp_dir_name} is to make temporary directory if [dir] is not given.
+    The default value of [mode] is [0o755].
+*)
+
+val remove : ?recursive:bool -> string -> unit
+(** [remove ?recursive path] removes file or directory located [path].
+    The default value of [recursive] is true, then removes all structure in [path] and removes [path].
+    If [recursive] is false and [path] targeted a directory, this function fails with {!Sys.Sys_error}.
+*)
