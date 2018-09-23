1fdae72d455b34022f2bdbaf2a54264d6fa2b460
derui <derutakayu@gmail.com>
2018-09-23 10:52:18 +0900

    feat: add new error

--- src/ocaml/rpc/errors.ml
+++ src/ocaml/rpc/errors.ml
@@ -7,4 +7,5 @@ module Filer = struct
   let already_exists = E.make ~message:"filer:Already_exists" (-2)
   let not_found_node = E.make ~message:"filer:Not_found_node" (-3)
   let not_directory = E.make ~message:"filer:Not_directory" (-4)
+  let not_found_workbench = E.make ~message:"not_found_workbench" (-5)
 end
