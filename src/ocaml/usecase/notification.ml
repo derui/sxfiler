05abd5bcf5228c662ed13208f4d54f5e1b82e992
derui <derutakayu@gmail.com>
2018-09-28 14:38:02 +0900

    feat: add use case to notify any message

---
+++ src/ocaml/usecase/notification.ml
@@ -0,0 +1,28 @@
+(** This module provides use cases to send notification. *)
+
+module T = Sxfiler_domain
+
+(** Notify a object. *)
+module Notify = struct
+  module Type = struct
+    type input = {notification : T.Notification.body}
+    type output = unit
+    type error = unit
+  end
+
+  module type S = sig
+    include module type of Type
+
+    include
+      Common.Usecase with type input := input and type output := output and type error := error
+  end
+
+  module Make (NF : T.Notification.Factory) (NS : T.Notification_service.S) : S = struct
+    include Type
+
+    let execute (params : input) =
+      let notification = NF.create params.notification in
+      let open Lwt in
+      NS.send notification >>= return_ok
+  end
+end
