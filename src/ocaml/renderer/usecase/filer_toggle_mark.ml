9e3e2c65a348f12801710c9b4ccf0d156aef4184
derui <derutakayu@gmail.com>
2018-10-28 08:48:28 +0900

    fix: Fix toggle mark to move cursor simultaneously

--- src/ocaml/renderer/usecase/filer_toggle_mark.ml
+++ src/ocaml/renderer/usecase/filer_toggle_mark.ml
@@ -8,4 +8,5 @@ let create () = ()
 
 let execute () dispatcher =
   let module D = (val dispatcher : C.Dispatcher_intf.Instance) in
-  Lwt.return @@ D.(Dispatcher.dispatch this @@ C.Message.Toggle_mark)
+  D.(Dispatcher.dispatch this @@ C.Message.Toggle_mark) ;
+  D.(Dispatcher.dispatch this C.Message.Move_cursor_to_next) |> Lwt.return
