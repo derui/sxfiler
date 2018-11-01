fad2e94ab695028c1ff057e3131b9ece4d365066
derui <derutakayu@gmail.com>
2018-11-02 00:02:39 +0900

    feat: add simple function to extract result

--- src/ocaml/core/result.ml
+++ src/ocaml/core/result.ml
@@ -1,6 +1,9 @@
 (** [to_option result] one-way convert to option. *)
 let to_option = function Ok v -> Some v | Error _ -> None
 
+(** [get r] returns a value when [r] is {!Ok}, otherwise raise exception or else.  *)
+let get = function Ok v -> v | Error _ -> failwith "Can not get value from result"
+
 include Monad.Make2 (struct
     type ('a, 'b) t = ('a, 'b) result
 
