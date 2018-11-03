c09e76d958b7ad51781b39477f25d848582bc04d
derui <derutakayu@gmail.com>
2018-11-04 08:55:24 +0900

    test: add tests for delete plan

--- test/usecase/plan_filer_test.ml
+++ test/usecase/plan_filer_test.ml
@@ -15,7 +15,7 @@ let dir_stat =
 let node_base ~id ?(full_path = Path.of_string "foo") ?(stat = stat_base) ?(link_path = None) () =
   D.Node.make ~id ~full_path ~stat ~link_path
 
-let test_set =
+let move_plan_test_set =
   [ Alcotest_lwt.test_case "move_nodes plan issue error when workbench not found" `Quick
       (fun _ () ->
          let module WR = struct
@@ -99,5 +99,50 @@ let test_set =
                 ; source = [Fun.(List.hd %> node_to_conflict) nodes]
                 ; dest = [Fun.(List.hd %> node_to_conflict) nodes] }
             in
-            Alcotest.(check (of_pp Fmt.nop)) "plan" expected plan ;
+            Alcotest.(check (of_pp D.Plan.pp)) "plan" expected plan ;
             Lwt.return_unit ) ]
+
+let delete_plan_test_set =
+  [ Alcotest_lwt.test_case "delete_nodes plan issue error when workbench not found" `Quick
+      (fun _ () ->
+         let module WR = struct
+           let resolve _ = Lwt.return_none
+           let store _ = assert false
+           let remove _ = assert false
+         end in
+         let module Usecase = U.Plan.Filer.Delete_nodes.Make (WR) in
+         let%lwt result = Usecase.execute {workbench_id = Uuidm.v4_gen (Random.get_state ()) ()} in
+         Alcotest.(check @@ result (of_pp Fmt.nop) (of_pp Fmt.nop))
+           "not found workbench"
+           (Error `Not_found_wb)
+           result ;
+         Lwt.return_unit )
+  ; Alcotest_lwt.test_case "get the plan to delete nodes" `Quick (fun _ () ->
+        let nodes = [node_base ~id:"bar" ~stat:dir_stat ()] in
+        let source_filer =
+          D.Filer.make ~id:"foo"
+            ~location:Path.(of_string "/bar")
+            ~nodes
+            ~history:D.Location_history.(make ())
+            ~sort_order:D.Types.Sort_type.Name
+        in
+        let id = Uuidm.v4_gen (Random.get_state ()) () in
+        let env = D.Workbench.{source = source_filer; nodes; dest = source_filer} in
+        let module WR = struct
+          let resolve _ = Lwt.return_some (D.Workbench.make ~id ~env ~corrections:[])
+          let store _ = assert false
+          let remove _ = assert false
+        end in
+        let module Usecase = U.Plan.Filer.Delete_nodes.Make (WR) in
+        let%lwt ret = Usecase.execute {workbench_id = Uuidm.v4_gen (Random.get_state ()) ()} in
+        let plan = Result.get ret in
+        let expected =
+          D.Plan.
+            { workbench_id = id
+            ; source = [Fun.(List.hd %> node_to_remain) nodes]
+            ; dest = [Fun.(List.hd %> node_to_delete) nodes] }
+        in
+        Alcotest.(check (of_pp D.Plan.pp)) "plan" expected plan ;
+        Lwt.return_unit ) ]
+
+let test_set = move_plan_test_set @ delete_plan_test_set
