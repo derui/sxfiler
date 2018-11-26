29103c27ef5829287b392491c01e9c1e5d982a0f
derui <derutakayu@gmail.com>
2018-11-26 23:45:20 +0900

    feat: add factory for file stat

---
+++ src/ts/domain/file-stat-factory.ts
@@ -0,0 +1,74 @@
+import { Capability, FileStat, Mode } from "./file-stat";
+
+export interface FactoryArg {
+  mode: string;
+  uid: number;
+  gid: number;
+  atime: string;
+  ctime: string;
+  mtime: string;
+  size: string;
+  isDirectory: boolean;
+  isFile: boolean;
+  isSymlink: boolean;
+}
+
+/**
+ * Convert from bits of capability to capability value object.
+ * @param bits
+ */
+function bitsToCapability(bits: number): Capability {
+  // tslint:disable:no-bitwise
+  const writable = (bits & 0o2) === 0o2;
+  return {
+    writable,
+    readable: (bits & 0o4) === 0o4,
+    executable: (bits & 0o1) === 0o1,
+  };
+  // tslint:enable:no-bitwise
+}
+
+/**
+ * convert string that contains capabilities
+ * @param str mode string
+ */
+function toMode(str: string): Mode {
+  const capabilities = Number(str);
+  if (Number.isInteger(capabilities)) {
+    throw new Error(`Invalid number format: ${str}`);
+  }
+
+  // tslint:disable:no-bitwise
+  const ownerBits = (capabilities & 0o700) >> 6;
+  const groupBits = (capabilities & 0o70) >> 3;
+  const othersBits = capabilities & 0o7;
+  // tslint:enable:no-bitwise
+
+  return {
+    owner: bitsToCapability(ownerBits),
+    group: bitsToCapability(groupBits),
+    others: bitsToCapability(othersBits),
+  };
+}
+
+// Factory of FileStat
+export class FileStatFactory {
+  /**
+   * create Filer
+   * @param arg arguments of factory
+   */
+  public static create(arg: FactoryArg) {
+    return new FileStat(
+      toMode(arg.mode),
+      arg.uid,
+      arg.gid,
+      new Date(arg.atime),
+      new Date(arg.ctime),
+      new Date(arg.mtime),
+      arg.size,
+      arg.isDirectory,
+      arg.isFile,
+      arg.isSymlink
+    );
+  }
+}
--- src/ts/domain/file-stat.ts
+++ src/ts/domain/file-stat.ts
@@ -1,15 +1,28 @@
 // define type and class of file stat
+export interface Capability {
+  readonly writable: boolean;
+  readonly readable: boolean;
+  readonly executable: boolean;
+}
+
+export interface Mode {
+  readonly owner: Capability;
+  readonly group: Capability;
+  readonly others: Capability;
+}
 
 // information object of file
-export interface FileStat {
-  mode: string;
-  uid: number;
-  gid: number;
-  atime: string;
-  ctime: string;
-  mtime: string;
-  size: string;
-  isDirectory: boolean;
-  isFile: boolean;
-  isSymlink: boolean;
+export class FileStat {
+  constructor(
+    public readonly mode: Mode,
+    public readonly uid: number,
+    public readonly gid: number,
+    public readonly atime: Date,
+    public readonly ctime: Date,
+    public readonly mtime: Date,
+    public readonly size: string,
+    public readonly isDirectory: boolean,
+    public readonly isFile: boolean,
+    public readonly isSymlink: boolean
+  ) {}
 }
