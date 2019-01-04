1045193cec97021fb1c8912c63616f430b961fa0
derui <derutakayu@gmail.com>
2019-01-04 08:47:36 +0000

    chore: drop react-create-app-typescript

---
+++ src/ts/components/file-item-timestamp.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+interface Prop {
+  timestamp: Date;
+}
+
+/**
+ * format Date to display in item
+ */
+function format(timestamp:Date) : String {
+  const year = `${timestamp.getFullYear()}`.padStart(4, "0");
+  const month = `${timestamp.getMonth() + 1}`.padStart(2, "0");
+  const date = `${timestamp.getDate()}`.padStart(2, "0");
+
+  return `${year}/${month}/${date}`;
+}
+
+const FileItemTimestamp : React.SFC<Prop> = prop => {
+  const date = format(prop.timestamp);
+  return (
+    <span className="fp-FileItem_Timestamp">{date}</span>
+  );
+};
+
+export default FileItemTimestamp;
