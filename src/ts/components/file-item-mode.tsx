504678f74788918859c49fd3682f76ce4ae89a01
derui <derutakayu@gmail.com>
2019-01-04 08:18:35 +0000

    fix: rewrite component from js_of_ocaml

--- src/ts/components/file-item-mode.tsx
+++ src/ts/components/file-item-mode.tsx
@@ -1,32 +1,32 @@
 import * as React from "react";
 import {Capability, FileStat} from "../domain/file-stat";
 
-  function capabilityToString(cap: Capability) {
-    const readable = cap.readable ? "r" : "-";
-    const writable = cap.writable ? "w" : "-";
-    const executable = cap.executable ? "x" : "-";
+function capabilityToString(cap: Capability) {
+  const readable = cap.readable ? "r" : "-";
+  const writable = cap.writable ? "w" : "-";
+  const executable = cap.executable ? "x" : "-";
 
-    return `${readable}${writable}${executable}`;
+  return `${readable}${writable}${executable}`;
+}
+
+/**
+ * convert mode to string
+ */
+function modeToString(stat: FileStat) : string {
+  var state = "=";
+
+  if (stat.isDirectory) {
+    state = "d";
+  } else if (stat.isSymlink) {
+    state = "l";
   }
 
-  /**
-   * convert mode to string
-   */
-  function modeToString(stat: FileStat) : string {
-    var state = "=";
+  const owner = capabilityToString(stat.mode.owner);
+  const group = capabilityToString(stat.mode.group);
+  const other = capabilityToString(stat.mode.other);
 
-    if (stat.isDirectory) {
-      state = "d";
-    } else if (stat.isSymlink) {
-      state = "l";
-    }
-
-    const owner = capabilityToString(stat.mode.owner);
-    const group = capabilityToString(stat.mode.group);
-    const other = capabilityToString(stat.mode.other);
-
-    return `${state}${owner}${group}${other}`;
-  }
+  return `${state}${owner}${group}${other}`;
+}
 
 interface Prop {
   stat: FileStat;
