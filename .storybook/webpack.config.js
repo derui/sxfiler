288d6ca6576bfda0df592799a0ace5fd610a988b
derui <derutakayu@gmail.com>
2019-03-06 23:58:46 +0900

    chore: fix to run with ejected configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,44 @@
+const _RuleSet = require("webpack/lib/RuleSet");
+
 const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
 
+// functions from cra-config.js of storybook
+const cssExtensions = ['.css', '.scss', '.sass'];
+const cssModuleExtensions = ['.module.css', '.module.scss', '.module.sass'];
+
+const getRules = extensions => rules => rules.reduce((craRules, rule) => {
+  // If at least one extension satisfies the rule test, the rule is one
+  // we want to extract
+  if (rule.test && extensions.some((0, _RuleSet.normalizeCondition)(rule.test))) {
+    // If the base test is for extensions, return early
+    return craRules.concat(rule);
+  } // Get any rules contained in rule.oneOf
+
+
+  if (!rule.test && rule.oneOf) {
+    craRules.push(...getRules(extensions)(rule.oneOf));
+  } // Get any rules contained in rule.rules
+
+
+  if (!rule.test && rule.rules) {
+    craRules.push(...getRules(extensions)(rule.rules));
+  }
+
+  return craRules;
+}, []);
+
+const getStyleRules = getRules(cssExtensions.concat(cssModuleExtensions));
+
 module.exports = (baseConfig, env, config) => {
+  craConfig = require("../config/webpack.config.js")(baseConfig.mode);
+  const styleRules = getStyleRules(craConfig.module.rules)
+  config = Object.assign(
+    config, {
+      module: Object.assign({}, config.module, {
+        rules: [...config.module.rules, ...styleRules]
+      }),
+    });
+
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
