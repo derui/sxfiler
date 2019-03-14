63c0e650834b275b77c42c399c9dc84b7f46f9ab
derui <derutakayu@gmail.com>
2019-03-14 23:41:15 +0900

    [wip] implement resize-sensor

---
+++ src/ts/libs/resize-sensor/observer.ts
@@ -0,0 +1,40 @@
+/**
+ * this module defines functions to manage resize observer.
+ * Only one function as callback to an element, and have only one observer in application.
+
+ Do not this module directory in application, so use ResizeSensor component in it.
+ */
+
+import ResizeObserver from "resize-observer-polyfill";
+
+export type ResizeCallback = (entry: ResizeObserverEntry, unobserve: () => void) => void;
+
+const observer = new ResizeObserver(observerCallback);
+const callbackMap: Map<Element, ResizeCallback> = new Map();
+
+function observerCallback(entries: ResizeObserverEntry[], observer: ResizeObserver) {
+  for (let entry of entries) {
+    let handler = callbackMap.get(entry.target);
+
+    if (handler) {
+      handler(entry, () => observer.unobserve(entry.target));
+    }
+  }
+}
+
+/**
+ * Add an element to observer and callback to handle the event when the element resized.
+ */
+export function observe(element: Element, callback: ResizeCallback) {
+  observer.observe(element);
+
+  callbackMap.set(element, callback);
+}
+
+/**
+ * Remove the element from observer.
+ */
+export function unobserve(element: Element) {
+  observer.unobserve(element);
+  callbackMap.delete(element);
+}
