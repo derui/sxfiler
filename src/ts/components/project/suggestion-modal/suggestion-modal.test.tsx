bf232ad87f5560ddf7f6f45afb98f522ee057615
derui <derutakayu@gmail.com>
2019-05-30 11:59:43 +0900

    feat: allow to show suggestions in the modal

--- src/ts/components/project/suggestion-modal/suggestion-modal.test.tsx
+++ src/ts/components/project/suggestion-modal/suggestion-modal.test.tsx
@@ -1,9 +1,63 @@
 import * as React from "react";
-import renderer from "react-test-renderer";
 import { mount } from "enzyme";
+import * as Modal from "./suggestion-modal";
+import { createSuggestion, SuggestionKind } from "../../../domains/task-suggestion";
+import { Component as OverwriteSuggestion } from "./overwrite-suggestion";
+import { Component as RenameSuggestion } from "./rename-suggestion";
 
 describe("Project", () => {
   describe("Suggestion Modal", () => {
-    it("render", () => {});
+    it("do not render when no any suggestion", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            focusedSuggestion: 0,
+            suggestions: [],
+            onReply: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find(OverwriteSuggestion)).toHaveLength(0);
+      expect(tree.find(RenameSuggestion)).toHaveLength(0);
+    });
+
+    it("render with overwrite suggestion", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            focusedSuggestion: 0,
+            suggestions: [createSuggestion({ kind: SuggestionKind.Overwrite, nodeName: "foo" })],
+            onReply: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find(OverwriteSuggestion)).toHaveLength(1);
+      expect(tree.find(RenameSuggestion)).toHaveLength(0);
+    });
+    it("render with rename suggestion", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            focusedSuggestion: 0,
+            suggestions: [createSuggestion({ kind: SuggestionKind.Rename, nodeName: "foo" })],
+            onReply: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find(OverwriteSuggestion)).toHaveLength(0);
+      expect(tree.find(RenameSuggestion)).toHaveLength(1);
+    });
   });
 });
--- src/ts/components/project/suggestion-modal/suggestion-modal.tsx
+++ src/ts/components/project/suggestion-modal/suggestion-modal.tsx
@@ -1,16 +1,32 @@
 import * as React from "react";
 import * as Modal from "../../ui/modal/modal";
-import { Suggestions } from "../../../domains/task-suggestion";
+import { Suggestion, SuggestionKind } from "../../../domains/task-suggestion";
+import * as OverwriteSuggestion from "./overwrite-suggestion";
+import * as RenameSuggestion from "./rename-suggestion";
+import { ReplyPayload } from "../../../domains/task-reply";
 
 const style: Modal.ModalClassNames = require("./suggestion-modal.module.scss");
 
 type ContainerProps = {
   className?: string;
-  suggestions: Suggestions;
+  focusedSuggestion: number;
+  suggestions: Suggestion[];
+  onReply: (reply: ReplyPayload) => void;
 };
 
-const Container: React.SFC<ContainerProps> = ({ className, suggestions }) => {
-  return <div className={className} />;
+const makeSuggestionPanel = (index: number, suggestions: Suggestion[], handleReply: (reply: ReplyPayload) => void) => {
+  return suggestions.map((v, i) => {
+    switch (v.kind) {
+      case SuggestionKind.Overwrite:
+        return <OverwriteSuggestion.Component key={i} selected={index === i} suggestion={v} onReply={handleReply} />;
+      case SuggestionKind.Rename:
+        return <RenameSuggestion.Component key={i} selected={index === i} suggestion={v} onReply={handleReply} />;
+    }
+  });
+};
+
+const Container: React.FC<ContainerProps> = ({ className, suggestions, focusedSuggestion, onReply }) => {
+  return <div className={className}>{makeSuggestionPanel(focusedSuggestion, suggestions, onReply)}</div>;
 };
 
 export type Props = Modal.Props<ContainerProps>;
