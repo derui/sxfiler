596da4589da2eb60f2c3a6f684cc17ab86b32ec1
derui <derutakayu@gmail.com>
2019-06-29 08:56:18 +0900

    fix: avoid to unnecessary reset state of the filer

---
+++ src/ts/usecases/filer/filer-updated.test.ts
@@ -0,0 +1,26 @@
+import * as actions from "../../actions/filer";
+import * as U from "./filer-updated";
+import { createFiler } from "../../domains/filer";
+
+describe("UseCase", () => {
+  describe("Filer", () => {
+    describe("Filer Updated", () => {
+      it("execute action to load filer from server state", () => {
+        const useCase = U.createUseCase();
+        const dispatcher = {
+          dispatch: jest.fn(),
+        };
+        const filer = createFiler({
+          id: "id",
+          name: "name",
+          location: "/",
+          items: [],
+          currentCursorIndex: 0,
+        });
+
+        useCase.execute(dispatcher, { filer });
+        expect(dispatcher.dispatch).toBeCalledWith(actions.actions.load({ filer }));
+      });
+    });
+  });
+});
--- src/ts/usecases/filer/filer-updated.ts
+++ src/ts/usecases/filer/filer-updated.ts
@@ -13,7 +13,7 @@ export type UseCase = UseCaseLike<Actions, Args>;
 export const createUseCase = (): UseCase => {
   return {
     execute(dispatcher: Dispatcher<Actions>, { filer }: Args) {
-      dispatcher.dispatch(actions.update({ filer }));
+      dispatcher.dispatch(actions.load({ filer }));
     },
   };
 };
