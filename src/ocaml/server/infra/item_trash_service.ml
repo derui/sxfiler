8993277a4203479ebe92ea562e1d367524af9038
derui <derutakayu@gmail.com>
2019-06-30 09:22:24 +0900

    fix: allow to delete the directory

--- src/ocaml/server/infra/item_trash_service.ml
+++ src/ocaml/server/infra/item_trash_service.ml
@@ -4,15 +4,6 @@ open Sxfiler_core
 module C = Sxfiler_server_core
 module D = Sxfiler_domain
 
-(* delete whole directory *)
-let rec delete_dir path =
-  let paths = Sys.readdir path |> Array.to_list in
-  let dirs = List.filter Sys.is_directory paths
-  and files = List.filter Fun.(Sys.is_directory %> not) paths in
-  let%lwt () = Lwt_list.iter_p (fun path -> Sys.remove path |> Lwt.return) files in
-  let%lwt () = Lwt_list.iter_p delete_dir dirs in
-  Unix.rmdir path |> Lwt.return
-
 module Make
     (NS : Notification_service.S)
     (MF : Message_notification_factory.S)
@@ -35,11 +26,7 @@ module Make
     Lwt_list.iter_p
       (fun item ->
          let path = item.D.File_item.full_path |> Path.to_string in
-         let%lwt () =
-           match item.D.File_item.stat.is_directory with
-           | true -> delete_dir path
-           | false -> File.remove path |> Lwt.return
-         in
+         let%lwt () = File.remove path |> Lwt.return in
          up_count () ;%lwt
          Log.debug (fun m -> m "Delete item: [%s]" path) ;%lwt
          MF.create ~level:Message_notification.Info ~body:Printf.(sprintf "Delete item: [%s]" path)
