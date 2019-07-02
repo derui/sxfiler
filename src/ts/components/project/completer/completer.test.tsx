a026ca0ed6441188057e5d8e93160ef4bf0614b7
derui <derutakayu@gmail.com>
2019-07-02 23:35:47 +0900

    feat: allow to show candidates and input as modal

---
+++ src/ts/components/project/completer/completer.test.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import { mount } from "enzyme";
+import * as Modal from "./completer";
+import * as ListItem from "../../ui/list-item/list-item";
+import { createCandidate } from "../../../domains/candidate";
+
+describe("Project", () => {
+  describe("Completer", () => {
+    it("render correctly", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            title: "test",
+            selectedItemIndex: -1,
+            items: [],
+            onInput: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find("input")).toHaveLength(1);
+    });
+
+    it("render with a candidate", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            title: "test",
+            selectedItemIndex: -1,
+            items: [createCandidate({ id: "id", value: "value", start: 0, length: 0 })],
+            onInput: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find(ListItem.Component)).toHaveLength(1);
+    });
+
+    it("render with candidates", () => {
+      const root = document.createElement("div");
+      const tree = mount(
+        <Modal.Component
+          dialogRoot={root}
+          opened={true}
+          container={{
+            title: "test",
+            selectedItemIndex: -1,
+            items: [
+              createCandidate({ id: "id", value: "value", start: 0, length: 0 }),
+              createCandidate({ id: "id2", value: "value2", start: 0, length: 0 }),
+            ],
+            onInput: () => {},
+          }}
+        />
+      );
+
+      expect(tree.find(ListItem.Component)).toHaveLength(2);
+    });
+
+    it("call callback when change input", () => {
+      const root = document.createElement("div");
+      const promise = new Promise(resolve => {
+        const tree = mount(
+          <Modal.Component
+            dialogRoot={root}
+            opened={true}
+            container={{
+              title: "test",
+              selectedItemIndex: -1,
+              items: [
+                createCandidate({ id: "id", value: "value", start: 0, length: 0 }),
+                createCandidate({ id: "id2", value: "value2", start: 0, length: 0 }),
+              ],
+              onInput: v => {
+                expect(v).toEqual("foo");
+                resolve();
+              },
+            }}
+          />
+        );
+
+        tree.find('input[type="text"]').simulate("change", {
+          target: {
+            value: "foo",
+          },
+        });
+      });
+
+      return promise;
+    });
+  });
+});
---
+++ src/ts/components/project/completer/completer.tsx
@@ -0,0 +1,134 @@
+import * as React from "react";
+import * as Modal from "../../ui/modal/modal";
+import * as List from "../../ui/list/list";
+import * as ListItem from "../../ui/list-item/list-item";
+import { CSSTransition } from "react-transition-group";
+import { Candidate } from "../../../domains/candidate";
+
+const style: ClassNames = require("./suggestion-modal.module.scss");
+
+type ClassNames = Modal.ModalClassNames & {
+  title: string;
+  itemContainer: string;
+  inputContainer: string;
+  label: string;
+  input: string;
+  list: string;
+  listItem: string;
+  rootAnimationExit: string;
+  rootAnimationExitActive: string;
+  rootAnimationEnter: string;
+  rootAnimationEnterActive: string;
+  overlayAnimationEnter: string;
+  overlayAnimationEnterActive: string;
+};
+
+export type OverlayProps = { className?: string };
+
+type Item = Candidate;
+
+export type ContainerProps = {
+  className?: string;
+  title: string;
+  items: Item[];
+  selectedItemIndex: number;
+  onInput: (input: string) => void;
+};
+
+type ContainerContextProps = ContainerProps & {
+  opened: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+};
+
+type OverlayContextProps = OverlayProps & {
+  opened: boolean;
+};
+
+const Overlay: React.FC<OverlayContextProps> = ({ className, opened }) => {
+  return (
+    <CSSTransition
+      in={opened}
+      timeout={100}
+      unmountOnExit={true}
+      classNames={{
+        enter: style.overlayAnimationEnter,
+        enterActive: style.overlayAnimationEnterActive,
+      }}
+    >
+      {() => <div className={className} />}
+    </CSSTransition>
+  );
+};
+
+const handleChange = (cb: (input: string) => void, cb2: (input: string) => void) => (
+  e: React.ChangeEvent<HTMLInputElement>
+): void => {
+  cb(e.target.value || "");
+  cb2(e.target.value || "");
+};
+
+/**
+ * Make list that contains completion items
+ */
+const makeList = (items: Item[], index: number) => {
+  const listItems = items.map((v, i) => (
+    <ListItem.Component className={style.listItem} selected={i === index} key={v.id}>
+      {v.value}
+    </ListItem.Component>
+  ));
+  return <List.Component className={style.list}>{listItems}</List.Component>;
+};
+
+const Container: React.FC<ContainerContextProps> = ({
+  className,
+  selectedItemIndex,
+  items,
+  onInput,
+  title,
+  opened,
+  onClose,
+  onOpen,
+}) => {
+  const [state, setState] = React.useState("");
+
+  return (
+    <CSSTransition
+      in={opened}
+      timeout={200}
+      onEnter={onOpen}
+      onExited={onClose}
+      classNames={{
+        enter: style.rootAnimationEnter,
+        enterActive: style.rootAnimationEnterActive,
+        exit: style.rootAnimationExit,
+        exitActive: style.rootAnimationExitActive,
+      }}
+    >
+      {() => {
+        return (
+          <div className={className}>
+            <h4 className={style.title}>{title}</h4>
+            <section className={style.itemContainer}>
+              <div className={style.inputContainer}>
+                <label className={style.label}>
+                  Input:{" "}
+                  <input className={style.input} type="text" onChange={handleChange(onInput, setState)} value={state} />
+                </label>
+              </div>
+              <List.Component className={style.list}>{makeList(items, selectedItemIndex)}</List.Component>
+            </section>
+          </div>
+        );
+      }}
+    </CSSTransition>
+  );
+};
+
+export type Props = Modal.Props<ContainerProps, OverlayProps>;
+
+export const Component = Modal.createComponent({
+  classNames: style,
+  container: Container,
+  overlay: Overlay,
+});
