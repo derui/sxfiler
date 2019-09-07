b2f931c17eba12e2d34c5561548073c988974639
derui <derutakayu@gmail.com>
2019-09-07 09:06:01 +0900

    chore: use CLI instead of raw module

---
+++ scripts/prepare-build.js
@@ -0,0 +1,25 @@
+'use strict';
+
+process.env.NODE_ENV = 'production';
+
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+require('../config/env');
+
+const path = require('path');
+const fs = require('fs-extra');
+const paths = require('../config/paths');
+
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml,
+  });
+}
+
+fs.emptyDirSync(paths.appBuild);
+// Merge with the public folder
+copyPublicFolder();
