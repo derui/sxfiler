73fbe30a3f4814cca96dd4b8cc9138d024bdae71
derui <derutakayu@gmail.com>
2019-09-28 09:10:41 +0900

    feat: Run websocket server with unused port

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -42,6 +42,7 @@ module.exports = function(webpackEnv) {
   const env = getClientEnvironment(publicUrl);
 
   return {
+    target: 'electron-renderer',
     mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
     // Stop compilation early in production
     bail: isEnvProduction,
--- public/electron-starter.js
+++ public/electron-starter.js
@@ -3,9 +3,39 @@ const electron = require('electron');
 const path = require('path');
 const spawn = require('child_process').spawn;
 const os = require('os');
+const net = require('net');
 
 if (require.main !== module) {
   process.exit(1);
+}
+
+function checkAvailablePort(port) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.unref();
+    server.on('error', reject);
+    server.listen({ port }, () => {
+      const { port } = server.address();
+      server.close(() => {
+        resolve(port);
+      });
+    });
+  });
+}
+
+// find available port
+async function findAvailablePort() {
+  const portRange = { min: 1024, max: 65535 };
+
+  for (let i = portRange.min; i <= portRange.max; i++) {
+    try {
+      return await checkAvailablePort(i);
+    } catch (error) {
+      if (error.code !== 'EADDRINUSE') {
+        throw error;
+      }
+    }
+  }
 }
 
 // get the directory to save user configuration
@@ -16,22 +46,30 @@ function getConfigDir() {
   return configDir;
 }
 
-// spawn a server
-function spawnServer(configDir) {
-  console.log('Launching server...');
-
+function setupConfigurations(configDir) {
   const configPath = path.join(configDir, 'config.json');
   if (!fs.existsSync(configPath)) {
     fs.copyFileSync(path.join(__dirname, 'defaults', 'config.json'), configPath);
   }
+
   const keyMapPath = path.join(configDir, 'keymap.json');
   if (!fs.existsSync(keyMapPath)) {
     fs.copyFileSync(path.join(__dirname, 'defaults', 'keymap.json'), keyMapPath);
   }
 
+  const dictDir = path.join(configDir, 'dict');
+  if (!fs.existsSync(dictDir)) {
+    fs.mkdirSync(dictDir);
+  }
+}
+
+// spawn a server
+function spawnServer(configDir, port) {
+  console.log('Launching server...');
+
   const server = spawn(
     path.join(`${__dirname}.unpacked`, 'sxfiler_server'),
-    [`--config=${configPath}`, '-d', path.join(configDir, 'dict')],
+    ['--config', configDir, '-d', path.join(configDir, 'dict'), '--port', port],
     {
       stdio: 'inherit',
     }
@@ -47,34 +85,49 @@ const configDir = getConfigDir();
 if (!fs.existsSync(configDir)) {
   fs.mkdirSync(configDir, { recursive: true });
 }
+setupConfigurations(configDir);
 
-const app = electron.app;
-const server = spawnServer(configDir);
+async function launch() {
+  const port = await findAvailablePort();
+  const app = electron.app;
 
-app.on('ready', () => {
-  const browserWindow = new electron.BrowserWindow({
-    height: 600,
-    width: 800,
-    resizable: true,
-    acceptFirstMouse: true,
-    webPreferences: undefined,
+  const server = spawnServer(configDir, port);
+  process.env.SERVER_URL = `ws://localhost:${port}`;
+
+  app.on('ready', () => {
+    const browserWindow = new electron.BrowserWindow({
+      height: 600,
+      width: 800,
+      resizable: true,
+      acceptFirstMouse: true,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
+
+    if (!browserWindow) {
+      console.error('Can not open window');
+      process.exit(1);
+    }
+
+    browserWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`);
+    browserWindow.focusOnWebView();
   });
 
-  if (!browserWindow) {
-    console.error('Can not open window');
-    process.exit(1);
-  }
+  app.on('window-all-closed', () => {
+    server.kill();
+    app.quit();
+  });
 
-  browserWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`);
-  browserWindow.focusOnWebView();
-});
+  app.on('quit', () => {
+    server.kill();
+    app.quit();
+  });
 
-app.on('window-all-closed', () => {
-  server.kill();
-  app.quit();
-});
+  electron.ipcMain.on('quit', () => {
+    server.kill();
+    app.quit();
+  });
+}
 
-electron.ipcMain.on('quit', () => {
-  server.kill();
-  app.quit();
-});
+launch();
