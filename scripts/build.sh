c4d0b7534682d5664067cc0675815d0314972231
derui <derutakayu@gmail.com>
2020-01-15 19:38:36 +0900

    chore: fix process for windows with new builder image

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,13 +6,13 @@ mkdir -p /opt/libs
 git clone https://github.com/derui/migemocaml /opt/libs/migemocaml
 git clone https://github.com/derui/jsonrpc-ocaml /opt/libs/jsonrpc-ocaml
 opam install -y /opt/libs/migemocaml /opt/libs/jsonrpc-ocaml
-opam install -y --deps-only -t ./sxfiler.opam
 
 if [[ $BUILD_MODE == "linux" ]]; then
+    opam install -y --deps-only -t ./sxfiler.opam
     dune build --display=short --profile release
 fi
 
 if [[ $BUILD_MODE == "windows" ]]; then
-    opam install -y --deps-only ./sxfiler-windows.opam
+    opam install -y -t --deps-only ./sxfiler-windows.opam
     dune build --display=short -x windows --profile windows
 fi
