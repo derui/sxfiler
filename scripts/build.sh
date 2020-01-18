5003c4d008021a5b45c8d12c416e12c0243bd0a8
derui <derutakayu@gmail.com>
2020-01-18 10:08:52 +0900

    chore: fix process to build windows version

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,9 +6,9 @@ mkdir -p /opt/libs
 git clone https://github.com/derui/migemocaml /opt/libs/migemocaml
 git clone https://github.com/derui/jsonrpc-ocaml /opt/libs/jsonrpc-ocaml
 opam install -y /opt/libs/migemocaml /opt/libs/jsonrpc-ocaml
+opam install -y --deps-only -t ./sxfiler.opam
 
 if [[ $BUILD_MODE == "linux" ]]; then
-    opam install -y --deps-only -t ./sxfiler.opam
     dune build --display=short --profile release
 fi
 
