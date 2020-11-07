d847f5ebbdc7b9fa78d45b234c1244dcc2c6c560
derui <derutakayu@gmail.com>
2020-11-07 10:20:58 +0900

    add event for file list

--- src/ts/commands/internal/filer/apply-file-events.ts
+++ src/ts/commands/internal/filer/apply-file-events.ts
@@ -25,6 +25,7 @@ export const createCommand = (): Command => {
   return {
     identifier,
     async execute(dispatcher: Dispatcher<Actions>, _: CommandState, payload: Payload) {
+      dispatcher.dispatch(actions.updateItemOrders(payload.fileListId, payload.itemOrders));
       dispatcher.dispatch(actions.applyEvents(payload.fileListId, payload.events));
     },
   };
